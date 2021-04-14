8828314180a92d963e10cf00545b46081ede2e55
vida <h.vida4471@gmail.com>
2021-04-14 21:15:04 +0430

    r

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -9,9 +9,7 @@ import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import { useHistory } from "react-router-dom";
-interface InputRef {
-  focus: HTMLInputElement;
-}
+
 const useStyles = makeStyles({
   root: {
     width: 600,
