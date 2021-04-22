3f5be42975dc865db0875cbf5d2d553923281846
vida <h.vida4471@gmail.com>
2021-04-22 10:03:44 +0430

    import

--- src/components/ViewProducts.tsx
+++ src/components/ViewProducts.tsx
@@ -2,30 +2,33 @@ import { useState, useEffect, useRef } from "react";
 
 import _ from "lodash";
 
-import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Checkbox,
+  Card,
+  CardContent,
+  Typography,
+  Tooltip,
+  IconButton,
+  Paper,
+  TextField,
+  Button,
+  makeStyles,
+} from "@material-ui/core";
+
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
+
 import EditModalFunction from "./EditeModal";
-import Checkbox from "@material-ui/core/Checkbox";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import Tooltip from "@material-ui/core/Tooltip";
-import IconButton from "@material-ui/core/IconButton";
+
+import { IrowItem, Iclases } from "../interface/interface";
 
 import "../../src/App.css";
-
-import { IrowItem } from "../interface/interface";
-import { Iclases } from "../interface/interface";
 
 export const View = (props): JSX.Element => {
   const classes: Iclases = useStyles({ darkMode: true });
@@ -284,7 +287,7 @@ export const View = (props): JSX.Element => {
         <Button
           className={classes.btn}
           variant="contained"
-          color="primary"
+          color="secondary"
           onClick={addOrder}
           innerRef={focusbtn}
           id="btnAdd"
