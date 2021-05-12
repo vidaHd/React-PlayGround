a8384db2f3531e67ee6b82c727986d71191e7d79
vida <h.vida4471@gmail.com>
2021-05-12 10:54:11 +0430

    jsConverTs

--- src/components/admin/Languege.tsx
+++ src/components/admin/Languege.tsx
@@ -4,7 +4,6 @@ const Languege = () => {
       <input type="radio" value="en" name="language" defaultChecked /> English
       <input type="radio" value="zh-hk" name="language" /> Persian
       <div className="navigationButtonsLeft"></div>
-      <button bsStyle="success">&lt; Bak</button>
     </div>
   );
 };
